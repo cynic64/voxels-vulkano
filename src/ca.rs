65cb3acab58a95379001a1bbeac1e8c3af6f373f
Steve Larsen <f3bl@gmx.com>
2018-11-03 17:01:26 +0100

    forgot to add ca.rs

---
+++ src/ca.rs
@@ -0,0 +1,110 @@
+extern crate rand;
+extern crate rayon;
+use self::rayon::prelude::*;
+
+pub struct CellA {
+    pub cells: Vec<u8>,
+    width: usize,
+    height: usize,
+    length: usize,
+    min_surv: u8,
+    max_surv: u8,
+    min_birth: u8,
+    max_birth: u8,
+    max_age: u8
+}
+
+impl CellA {
+    pub fn new ( width: usize, height: usize, length: usize, min_surv: u8, max_surv: u8, min_birth: u8, max_birth: u8 ) -> Self {
+        let cells = vec![0; width * height * length];
+        let max_age = 1;
+
+        Self {
+            cells,
+            width,
+            height,
+            length,
+            min_surv,
+            max_surv,
+            min_birth,
+            max_birth,
+            max_age,
+        }
+    }
+
+    pub fn randomize ( &mut self ) {
+        let cells = (0 .. self.width * self.height * self.length)
+            .map(|_| {
+                if rand::random() {
+                    1
+                } else {
+                    0
+                }
+            })
+            .collect();
+
+        self.cells = cells;
+    }
+
+    pub fn next_gen ( &mut self ) {
+        let new_cells = (0 .. self.width * self.height * self.length)
+            .into_par_iter()
+            .map(|idx| {
+                if (idx > self.width * self.height + self.width) && (idx < (self.width * self.height * self.length) - (self.width * self.height) - self.width - 1) {
+                    let cur_state = self.cells[idx];
+                    let neighbors = [
+                        self.cells[idx + (self.width * self.height) + self.width + 1],
+                        self.cells[idx + (self.width * self.height) + self.width    ],
+                        self.cells[idx + (self.width * self.height) + self.width - 1],
+                        self.cells[idx + (self.width * self.height)              + 1],
+                        self.cells[idx + (self.width * self.height)                 ],
+                        self.cells[idx + (self.width * self.height)              - 1],
+                        self.cells[idx + (self.width * self.height) - self.width + 1],
+                        self.cells[idx + (self.width * self.height) - self.width    ],
+                        self.cells[idx + (self.width * self.height) - self.width - 1],
+                        self.cells[idx                              + self.width + 1],
+                        self.cells[idx                              + self.width    ],
+                        self.cells[idx                              + self.width - 1],
+                        self.cells[idx                                           + 1],
+                        self.cells[idx                                           - 1],
+                        self.cells[idx                              - self.width + 1],
+                        self.cells[idx                              - self.width    ],
+                        self.cells[idx                              - self.width - 1],
+                        self.cells[idx - (self.width * self.height) + self.width + 1],
+                        self.cells[idx - (self.width * self.height) + self.width    ],
+                        self.cells[idx - (self.width * self.height) + self.width - 1],
+                        self.cells[idx - (self.width * self.height)              + 1],
+                        self.cells[idx - (self.width * self.height)                 ],
+                        self.cells[idx - (self.width * self.height)              - 1],
+                        self.cells[idx - (self.width * self.height) - self.width + 1],
+                        self.cells[idx - (self.width * self.height) - self.width    ],
+                        self.cells[idx - (self.width * self.height) - self.width - 1]
+                    ];
+
+                    let count: u8 = neighbors.iter().sum();
+
+                    if cur_state > 0 {
+                        if count >= self.min_surv && count <= self.max_surv {
+                            let new_state = cur_state + 1;
+                            if new_state > self.max_age {
+                                self.max_age
+                            } else {
+                                new_state
+                            }
+                        } else {
+                            0
+                        }
+                    } else if count >= self.min_birth && count <= self.max_birth {
+                        1
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            })
+            .collect();
+
+        self.cells = new_cells;
+    }
+}
