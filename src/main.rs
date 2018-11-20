a86dff48ec39ab0df3fe38755290766d81161c3d
Steve Larsen <f3bl@gmx.com>
2018-11-20 16:46:59 +0100

    brighter dim spots

--- src/main.rs
+++ src/main.rs
@@ -598,8 +598,11 @@ layout(location = 0) out vec4 f_color;
 const vec3 LIGHT = vec3(3.0, 1.0, 1.0);
 
 void main() {
-    float brightness = max(dot(normalize(v_normal), normalize(LIGHT)), 0.1);
-    f_color = v_color * brightness;
+    float brightness = dot(normalize(v_normal), normalize(LIGHT));
+    vec3 dark_color = v_color.xyz * 0.6;
+    vec3 regular_color = v_color.xyz;
+
+    f_color = vec4(mix(dark_color, regular_color, brightness), 1.0);
 }
 "]
     #[allow(dead_code)]
