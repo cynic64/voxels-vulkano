ebae104723943f96c79844045ad8887bb813be0a
Steve Larsen <f3bl@gmx.com>
2018-11-21 02:53:12 +0100

    It works! :o

--- src/sector.rs
+++ src/sector.rs
@@ -3,33 +3,58 @@ extern crate rand;
 
 use self::glm::*;
 
+#[rustfmt::skip]
+const VISIBLE_OFFSETS: [(i32, i32, i32); 27] = [
+    ( 1, -1, -1),
+    ( 1,  0, -1),
+    ( 1,  1, -1),
+    ( 0, -1, -1),
+    ( 0,  0, -1),
+    ( 0,  1, -1),
+    (-1, -1, -1),
+    (-1,  0, -1),
+    (-1,  1, -1),
+    ( 1, -1,  0),
+    ( 1,  0,  0),
+    ( 1,  1,  0),
+    ( 0, -1,  0),
+    ( 0,  0,  0),
+    ( 0,  1,  0),
+    (-1, -1,  0),
+    (-1,  0,  0),
+    (-1,  1,  0),
+    ( 1, -1,  1),
+    ( 1,  0,  1),
+    ( 1,  1,  1),
+    ( 0, -1,  1),
+    ( 0,  0,  1),
+    ( 0,  1,  1),
+    (-1, -1,  1),
+    (-1,  0,  1),
+    (-1,  1,  1),
+];
+
 pub fn get_near_mesh_indices(camera_position: &Vec3, camera_front: &Vec3) -> Vec<usize> {
     use super::SECTOR_SIDE_LEN;
     use super::SIZE;
 
     let world_size_chunks = SIZE / SECTOR_SIDE_LEN;
 
-    let base_x = (camera_position.x as usize) / SECTOR_SIDE_LEN;
-    // z and y are swapped, because for the ca z is up and down and y forward/backward whereas in the camera position it is swapped
-    let base_y = (camera_position.z as usize) / SECTOR_SIDE_LEN;
-    let base_z = (camera_position.y as usize) / SECTOR_SIDE_LEN;
+    let mut base_x = ((camera_position.x as usize) / SECTOR_SIDE_LEN) as i32;
+    let mut base_y = ((camera_position.z as usize) / SECTOR_SIDE_LEN) as i32;
+    let mut base_z = ((camera_position.y as usize) / SECTOR_SIDE_LEN) as i32;
+
+    base_x += round(camera_front.x);
+    base_y += round(camera_front.z);
+    base_z += round(camera_front.y);
 
     // again, the conversion from usize to i32 is kinda shitty :/
     let mut indices = Vec::new();
 
-    let offsets = vec![
-        (0, 0, 0),
-        (
-            round(camera_front.x),
-            round(camera_front.z),
-            round(camera_front.y),
-        ),
-    ];
-
-    for offset in offsets.iter() {
-        let x = ((base_x as i32) + offset.0) as usize;
-        let y = ((base_y as i32) + offset.1) as usize;
-        let z = ((base_z as i32) + offset.2) as usize;
+    for &offset in VISIBLE_OFFSETS.iter() {
+        let x = (base_x + offset.0) as usize;
+        let y = (base_y + offset.1) as usize;
+        let z = (base_z + offset.2) as usize;
 
         let linear_idx = z * world_size_chunks * world_size_chunks + y * world_size_chunks + x;
         indices.push(linear_idx);
