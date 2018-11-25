5e6fb4ed3b1a0fabc830c3edb08c340a778dad27
Steve Larsen <f3bl@gmx.com>
2018-11-25 02:15:19 +0100

    moved the light slightly

--- src/main.rs
+++ src/main.rs
@@ -692,7 +692,7 @@ layout(location = 1) in vec3 v_normal;
 
 layout(location = 0) out vec4 f_color;
 
-const vec3 LIGHT = vec3(3.0, 1.0, 1.0);
+const vec3 LIGHT = vec3(3.0, 2.0, 1.0);
 
 void main() {
     float brightness = dot(normalize(v_normal), normalize(LIGHT));
